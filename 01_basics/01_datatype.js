113decdaf2b8d2b6a23f34c6f01971d3439b8b97
salonimandale <salonimandale@gmail.com>
2025-08-01 07:09:44 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+3)
+
+let name="saloni"
+let age = 21
+let islogin=false
+
+
+// prmitive
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null=> standalone value / representation of empty value
+// undefined => still value is not assigned
+// symbol => unique
+
+
+// non primitive
+// object
+
+console.log(typeof null);
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail = "abc@gmial.com"
+var accoutPassword ="12345"
+accountCity = "miraj"
+
+// accountId=2  not allowed
+
+accountEmail="xyyz@gmail.com"
+accoutPassword="abcd"
+
+accountCity="sangli"
+console.log(accountId);
+console.table([accountId,accountEmail,accoutPassword,accountCity])
