5387a482007b06d385c89510361d7ff2bdd8a3fd
salonimandale <salonimandale@gmail.com>
2025-08-01 07:30:33 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionsandoperations.js
@@ -0,0 +1,26 @@
+let score ="33abc" 
+
+console.log(typeof score)
+
+let inNumbaer = Number(score)
+console.log(typeof inNumbaer);
+console.log(inNumbaer);
+
+// "33"=>33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" =>false
+// "saloni" => true
+
+
+let num = 33
+let stringnum = String(num)
+console.log(stringnum);
+console.log(typeof stringnum);
+
+
