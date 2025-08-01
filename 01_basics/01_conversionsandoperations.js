8afc394f0ff82226727a50be7dd1ca2041a4d19c
salonimandale <salonimandale@gmail.com>
2025-08-01 07:48:39 +0000

    operations discussed

--- 01_basics/01_conversionsandoperations.js
+++ 01_basics/01_conversionsandoperations.js
@@ -1,26 +1,68 @@
-let score ="33abc" 
+let score = "hitesh"
 
-console.log(typeof score)
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let inNumbaer = Number(score)
-console.log(typeof inNumbaer);
-console.log(inNumbaer);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-// "33"=>33
+
+// "33" => 33
 // "33abc" => NaN
-// true => 1
+//  true => 1; false => 0
 
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+let isLoggedIn = "hitesh"
 
-// "" =>false
-// "saloni" => true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
 
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
 
-let num = 33
-let stringnum = String(num)
-console.log(stringnum);
-console.log(typeof stringnum);
+let someNumber = 33
 
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
